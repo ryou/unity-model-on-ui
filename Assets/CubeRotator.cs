df20401f193302c5e122ddd753e95e1e0d039fba
ryou <Alelorshy@gmail.com>
2018-05-10 12:20:26 +0900

    モーダル開閉機能、3Dモデル配置

---
+++ Assets/CubeRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var addAngle = this.speed * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(addAngle, addAngle, addAngle) * this.transform.rotation;
+	}
+}
---
+++ Assets/ModalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModalController : MonoBehaviour {
+
+    private void Awake()
+    {
+        Hide();
+    }
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
